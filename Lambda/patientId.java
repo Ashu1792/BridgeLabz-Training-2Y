580010a5fea2cc02ee265f3a5fe299cfdb4538b3
Ashu1792 <ashupal112200@gmail.com>
2025-11-15 10:22:58 +0530

    delete all files

--- Lambda/patientId.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class patientId {
-    public static void main(String[] args) {
-        List<String> patientIDs = Arrays.asList("P001", "P002", "P003");
-        patientIDs.forEach(System.out::println); // Method reference instead of lambda
-    }
-}
